a48ce2876eaeb955c5031fb5f60f5eaf68894ec1
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-07 13:32:11 +0530

    [Sehajpreet] Add: program to display a calendar for a given month and year

---
+++ Calender.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Calender {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter month (1-12): ");
+        int month = sc.nextInt();
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
+
+        displayCalendar(month, year);
+    }
+
+    public static void displayCalendar(int month, int year) {
+        String[] monthNames = {"", "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"};
+        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (month == 2 && isLeapYear(year)) {
+            daysInMonth[2] = 29;
+        }
+
+        System.out.println("\n   " + monthNames[month] + " " + year);
+        System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+
+        int startDay = getStartDay(month, year);
+
+        for (int i = 0; i < startDay; i++) {
+            System.out.print("    ");
+        }
+
+        for (int day = 1; day <= daysInMonth[month]; day++) {
+            System.out.printf("%3d ", day);
+            if ((day + startDay) % 7 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static int getStartDay(int month, int year) {
+        int y = year - (14 - month) / 12;
+        int x = y + y / 4 - y / 100 + y / 400;
+        int m = month + 12 * ((14 - month) / 12) - 2;
+        return (1 + x + (31 * m) / 12) % 7;
+    }
+}
